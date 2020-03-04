4ab9f9551705cc08c2f90652c0de624b41913fc4
nader <nader.kirrish@gmail.com>
2020-03-03 20:15:41 -0500

    more functionality and data changes

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 //TODO add farming lvl based items
-//TODO get runs from args
 //TODO adjust all POH teles to be house tab plus scroll
 
 const fs = require("fs")
-const yargs = require("yargs")
 
 let contentData = fs.readFileSync("farm_db.json")
 let content = JSON.parse(contentData)
@@ -32,17 +30,45 @@ keyArr.forEach((locKey) => {
 runs.forEach((run) => {
     content.types[run].loc.forEach((location) => {
         let locData = content.locs[location]
-        let reqs = locData.req 
+        let reqs = locData.req
 
         if (!reqs || 
             (reqs.length == 1 && profile[reqs[0]]) || 
             (reqs.length == 2 && profile[reqs[0]] && profile.farming_lvl >= reqs[1])) {
-                patches.push(locData)
-                //TODO change this to allow selecting in array
-                if (!content.teleports[locData.tele[0]].items){
-                    inventory.push(content.teleports[locData.tele[0]])
-                } else {
- //TODO for each item add to inventory
+                //TODO Associate teleports with patches/patch types
+                // console.log(locData.tele[0])
+                // console.log(content.teleports[locData.tele[0]])
+                let index = 0
+                let complete = false
+                while (!complete) {
+                    console.log(locData.tele[index])
+                    reqs = content.teleports[locData.tele[index]].reqs || ''
+                    if (content.teleports[locData.tele[index]] &&
+                        (!reqs || 
+                        (reqs.length == 1 && profile[reqs[0]]) || 
+                        (reqs.length == 2 && profile[reqs[0]] && profile.farming_lvl >= reqs[1]))) {
+                        //TODO eventually merge locations + multiple run types as one object
+                        patches.push({
+                            "patch": run,
+                            "location": locData.name
+                        })
+                        if (content.teleports[locData.tele[index]].items) {
+                            content.teleports[locData.tele[index]].items.forEach((item) => {
+                                if (!inventory.includes(content.items[item].name)) {
+                                    inventory.push(content.items[item].name)
+                                }
+                            })
+                            complete = true;
+                            //TODO for each item add to inventory
+                        } else {
+                            if (!inventory.includes(content.teleports[locData.tele[0]].name)) {
+                                inventory.push(content.teleports[locData.tele[0]].name)
+                            }
+                            complete = true;
+                        }
+                    } else {
+                        index++
+                    }
                 }
         } else
             console.log("reqs not met for " + location)
